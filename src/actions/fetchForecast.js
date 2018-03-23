15ce87ab1ac3dfda66247f782d9f0d9d3e540d63
pavel <paveltextfree@gmail.com>
2018-03-23 16:17:27 +0300

    test

--- src/actions/fetchForecast.js
+++ src/actions/fetchForecast.js
@@ -1,7 +1,10 @@
 const fetchForecast = id => {
   //загружает прогноз погоды при клике на выбранный город
+
   return fetch(`/api/location/${id}`)
     .then(response => response.json())
+    .then(data => (console.log('data', data), data))
+
     .catch(err => {
       console.log(err)
       return {}
